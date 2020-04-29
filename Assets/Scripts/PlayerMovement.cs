3f02810aa012efa92e4641d6b7c1841e652570b3
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-30 02:14:57 +0300

    update refactor

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+    private CharacterController _characterController;
+    private PlayerDirection _playerDirection;
+
+    private Vector3 _velocity;
+    
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _playerDirection = GetComponent<PlayerDirection>();
+    }
+
+    void Update()
+    {
+        CalculateVelocity();
+
+        _characterController.Move(_velocity * Time.deltaTime);
+    }
+
+    private void CalculateVelocity()
+    {
+        _velocity= _playerDirection.Direction * _movementSpeed;
+    }
+   
+}
