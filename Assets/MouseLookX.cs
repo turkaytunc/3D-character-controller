afa83daf2efacbce328f324a81ca54c83b864972
Turkay Tunc <turkaytunc00@gmail.com>
2020-04-30 19:23:00 +0300

    update mouselook

---
+++ Assets/MouseLookX.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MouseLookX : MonoBehaviour
+{
+    [SerializeField] [Range(0.5f, 3f)] private float mouseSensivity;
+    void Update()
+    {
+        LookAtX();
+    }
+
+    private void LookAtX()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+
+
+        Quaternion quaternion = Quaternion.Euler(new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + (mouseX * mouseSensivity), transform.localEulerAngles.z));
+
+        transform.rotation = quaternion;
+    }
+}
