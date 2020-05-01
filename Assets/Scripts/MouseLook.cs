7c1f612928be492ee5b25dff6db0f49bf361b4af
Turkay Tunc <turkaytunc00@gmail.com>
2020-05-02 00:03:46 +0300

    refactor player input

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -2,12 +2,16 @@
 
 public class MouseLook : MonoBehaviour
 {
+    [Header("Mouse Sensivity")]
     [SerializeField] [Range(10f, 30f)] private float mouseSensivityXAxis = 10f;
     [SerializeField] [Range(10f, 30f)] private float mouseSensivityYAxis = 10f;
 
 
     [SerializeField] private Transform playerControllerTransform;
-    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private Transform playerCameraTransform;
+
+    [SerializeField] private float minCameraYAngle = -50f;
+    
 
     private Vector2 inputValue;
     private Vector3 tempAngles = Vector3.zero;
@@ -30,7 +34,7 @@ public class MouseLook : MonoBehaviour
 
         tempAngles.x -= inputValue.y;
         tempAngles.x = Mathf.Clamp(tempAngles.x, -50f, 50f);
-        playerCamera.transform.localRotation = Quaternion.Euler(tempAngles);
+        playerCameraTransform.localRotation = Quaternion.Euler(tempAngles);
     }
 
     private void GetPlayerInput()
